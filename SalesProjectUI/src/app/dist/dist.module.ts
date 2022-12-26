e0a60f6273144366caff81779c14760f41bbb62c
SRUTHIAKUTHOTA <sruthiakuthota@gmail.com>
2022-12-26 12:38:03 +0530

    Update dist.module.ts

--- SalesProjectUI/src/app/dist/dist.module.ts
+++ SalesProjectUI/src/app/dist/dist.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLinkActive } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,FormsModule
+    CommonModule,FormsModule,RouterLinkActive
   ]
 })
 export class DistModule { }
