8e705d35f2a267b3fe35498bb3b430cbac57e2c7
praveenkathroju <praveenkathroju4@gmail.com>
2022-12-26 17:24:41 +0530

    Distributor Details Query

---
+++ SalesProjectUI/src/app/common modules/syncfusion/SyncFusion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import {  TextBoxModule } from "@syncfusion/ej2-angular-inputs";
+
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        TextBoxModule,
+    ]
+})
+export class syncfusion { }
+
