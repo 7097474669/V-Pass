4bd79e22a59d190a842e68ddea60f037795fdbe1
John Akshay <akkenakuntaakshay@gmail.com>
2022-12-26 13:12:11 +0530

    Authorizer
    added Angular mat in the project

--- SalesProjectUI/src/app/authorizer/authorizer.module.ts
+++ SalesProjectUI/src/app/authorizer/authorizer.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthorizerRoutingModule } from './authorizer-routing.module';
 import { AuthorizerComponent } from './authorizer.component';
-
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,12 @@ import { AuthorizerComponent } from './authorizer.component';
   ],
   imports: [
     CommonModule,
-    AuthorizerRoutingModule
+    AuthorizerRoutingModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+
   ]
 })
 export class AuthorizerModule { }
