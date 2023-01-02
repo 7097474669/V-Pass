16f95253c706bf301108f1a7b6502ebbd2adbebc
Sai Yashwant <42904267+Saiyashwant06@users.noreply.github.com>
2023-01-02 10:59:15 +0530

    PUSHING
    ALL THE CHANGE & ADD WAREHOUSE IMAGE AT WAREHOUSE COMPO

--- SalesProjectUI/src/app/features/ware-house/ware-house.module.ts
+++ SalesProjectUI/src/app/features/ware-house/ware-house.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WareHouseRoutingModule } from './ware-house-routing.module';
-import { WareHouseComponent } from './ware-house.component';
+
 
 
 @NgModule({
   declarations: [
-    WareHouseComponent
+
   ],
   imports: [
     CommonModule,
